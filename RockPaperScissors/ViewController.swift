ba9824d3a8ebb79509350d4dca371b755453b12d
Livia Leneweaver <lleneweaver54@haverfordsd.net>
2022-04-25 10:43:04 -0400

    new changes

--- RockPaperScissors/ViewController.swift
+++ RockPaperScissors/ViewController.swift
@@ -8,13 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let move = Int.random(in: 1...3)
     @IBOutlet weak var computerImage: UIImageView!
+    @IBOutlet weak var rockImage: UIImageView!
+    @IBOutlet weak var paperImage: UIImageView!
+    @IBOutlet weak var scissorImage: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        func random ()
+        {
+             Int(rockImage) = 1
+            Int(paperImage) = 2
+            Int(scissorImage) = 3
+        }
+        
     }
+    
 }
 
 
